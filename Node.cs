cb4d7fe206e36be073eeae8f8cff10ba8a47aa91
pasit-GDM025 <pasit.pipat@mail.kmutt.ac.th>
2022-11-21 14:25:18 +0700

    Doneeee

---
+++ Node.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Node<T> where T : struct
+{
+    private T value;
+    private Node<T> next = null;
+
+    public Node(T value)
+    {
+        this.SetValue(value);
+    }
+
+    public void SetNext(Node<T> next)
+    {
+        this.next = next;
+    }
+
+    public Node<T> Next()
+    {
+        return this.next;
+    }
+
+    public T GetValue()
+    {
+        return this.value;
+    }
+
+    public void SetValue(T value)
+    {
+        this.value = value;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class ProgramNo2{
+    static void Main(string[]args){
+        char PollamaiSod = 'y';
+        Queue<int> S = new Queue<int>();
+        Queue<int> M = new Queue<int>();
+        Queue<int> L = new Queue<int>();
+        while(PollamaiSod == 'L'|| PollamaiSod == 'M'||PollamaiSod == 'S'||PollamaiSod =='y'){
+            PollamaiSod = Char.Parse(Console.ReadLine());
+            if(PollamaiSod == 'S'){
+                S.Push(3);
+            }
+            else if(PollamaiSod == 'M'){
+                S.Push(2);
+                for(int i=0;i<3;i++){
+                    M.Push(3);
+                }
+            }
+            else if(PollamaiSod == 'L'){
+                S.Push(1);
+                for(int i=0;i<2;i++){
+                    M.Push(2);
+                    for(int j=0;j<3;j++){
+                        L.Push(3);
+                    }
+                }
+            }
+        }
+        for(int i=0;i<S.GetLength();i++){
+            Console.WriteLine("{0}",S.Get(i));
+        }
+        for(int i=0;i<M.GetLength();i++){
+            Console.WriteLine("{0}",M.Get(i));
+        }
+        for(int i=0;i<L.GetLength();i++){
+            Console.WriteLine("{0}",L.Get(i));
+        }
+    }
+}
